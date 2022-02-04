ae9004e5d18ac89874ca2805f389ce3a20cac071
Mikhail Bagmet <m.bagmet@gmail.com>
2022-02-04 18:54:20 +0200

    Initial Commit

---
+++ AppleMusic/AppleMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicApp.swift
+//  AppleMusic
+//
+//  Created by Михаил on 04.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
