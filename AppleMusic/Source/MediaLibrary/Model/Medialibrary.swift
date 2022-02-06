af28790cc9da0a627c99b81b1592cccc09fb85ab
Mikhail Bagmet <m.bagmet@gmail.com>
2022-02-06 12:50:34 +0200

    Add Medialibrary model and json

---
+++ AppleMusic/Source/MediaLibrary/Model/Medialibrary.swift
@@ -0,0 +1,8 @@
+//
+//  Medialibrary.swift
+//  AppleMusic
+//
+//  Created by Михаил on 06.02.2022.
+//
+
+import Foundation
---
+++ AppleMusic/Source/MediaLibrary/Model/ModelData.swift
@@ -0,0 +1,8 @@
+//
+//  ModelData.swift
+//  AppleMusic
+//
+//  Created by Михаил on 06.02.2022.
+//
+
+import Foundation
